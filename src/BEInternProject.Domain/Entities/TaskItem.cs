d633956b54d032e90bf6c908dc1e57602de5536b
Nguyen Khanh Tam <tamnguyen54204@gmail.com>
2026-02-04 17:08:44 +0700

    create seed role and database sql script

---
+++ src/BEInternProject.Domain/Entities/TaskItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEInternProject.Domain.Entities
+{
+    public class TaskItem
+    {
+        public Guid TaskItemId { get; set; }
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+        public Guid AssigneeId { get; set; }
+        public DateOnly Deadline { get; set; }
+        public int Priority { get; set; }
+        public int Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
